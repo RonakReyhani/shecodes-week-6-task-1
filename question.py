2e854b21dc2f0ccfd27d90a2024615841e307a77
Nadia <reyhani.ronak@gmail.com>
2019-08-30 21:52:02 +0800

    Modified classes and methods

--- question.py
+++ question.py
@@ -1,37 +1,32 @@
-from keep_answers import answer_dictionary
-
 class Question:
-    def __init__(self,q_number,question_text,question_answers,valid_answers=None,next_question=None):
-        self.q_number= q_number
+    def __init__(self, q_number, question_text, question_answers, valid_answers=None , user_answers = None):
+        self.q_number = q_number
         self.question_text = question_text
-        self.question_answers= question_answers
-        self.next_question = next_question
-        self.valid_answers=[]
+        self.question_answers = question_answers
+        self.valid_answers = []
+        self.user_answers = user_answers
 
 # ask question from user
-def ask_question(self):
+    def ask_question(self):
         print(f'{self.q_number} {self.question_text}\n')
-        for answer_code,answer_text in self.question_answers.items():
+        for answer_code, answer_text in self.question_answers.items():
             print(f'{answer_code} {answer_text}')
-        self.get_user_answer()
-        self.print_user_answer()    
+            self.get_user_answer()
+            self.print_user_answer()
         print('\n')
 
 
 # show user answer after each question : 
-def get_user_answer(self):
+    def get_user_answer(self):
         #ToDo: add validator for user answer /check if code is in the valid_answers
         user_answer = input('Your answer is: ')
         return user_answer
-        
-def print_user_answer(self):        
-        print (f'Your answer for question {self.q_number} {self.question_text} is {user_answer}')
-                    
 
-#keep a dictionary of question text and user answers per question : ASk Ben/Hayley should I put this method in section or question class? 
-def keep_answers(self):
-    self.answer_dict = answer_dictionary() 
-    self.answer_dict.key = self.question_text
-    self.answer_dict.value = input()
-    self.answer_dict.add(self.answer_dict.key, self.answer_dict.value)
-    return self.answer_dict   
+    @property
+    def user_answer_text(self):
+            return self.question_answers[self.user_answers]  
+
+    def print_user_answer(self):
+        print(f'Your answer for question {self.q_number} {self.question_text} is {self.user_answers}')
+
+          
--- section.py
+++ section.py
@@ -2,31 +2,31 @@ from question import Question
 
 class Section:
     
-    def __init__ (self,sec_title,sec_description,question_list,first_question=None,qualifying_question=None,user_answers={}):
+    def __init__ (self,sec_title,sec_description,question_list, qualifying_question=None):
         
         self.sec_title = sec_title
         self.sec_description = sec_description
         self.qualifying_question = qualifying_question
         self.question_list = question_list
-        self.first_question = first_question
 
-
+    # check if user answer for qualify question is yes then run the section          
+    def ask_qualify_question(self):
+        if self.qualifying_question:
+            self.qualifying_question.ask_question()    # ask if these two will work when ask_question() and get_user_answer() are methods in Question
+        return self.qualifying_question.user_answer  
 
 
 # run method for each section
 
-def run_section(self):
+    def run_section(self):
         print(f'============================ {self.sec_title} ============================\n')
         print(f'{self.sec_description}\n')
         for question in self.question_list:
             question.ask_question()
-            question.get_user_answer()
             question.print_user_answer()
-            self.user_answers = question.keep_answers()
-        for key, value in self.user_answers.items():
-            print(f'your answer for {key} is {value}')
-        print ('Print Thank you messgae: Thanks for your time.bala bala bala....')
-        exit # how to add exit comand search for this          
-
+        for question in self.question_list:
+            print(f'your answer for {question.question_text} is [{question.user_answer}] {question.user_answer_text()}')
+        print('Print Thank you messgae: Thanks for your time.bala bala bala....')
+    
 
 
